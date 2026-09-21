5d6e69c942ad4887aed73405fe796cc47dfc9376
kseni <zaytseva9948@gamil.com>
2026-09-21 11:17:42 +0300

    Initial commit

---
+++ src/main/java/com/FromPegaToJava/SpringQ3/SpringQ3Application.java
@@ -0,0 +1,13 @@
+package com.FromPegaToJava.SpringQ3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQ3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQ3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/FromPegaToJava/SpringQ3/SpringQ3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FromPegaToJava.SpringQ3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQ3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
