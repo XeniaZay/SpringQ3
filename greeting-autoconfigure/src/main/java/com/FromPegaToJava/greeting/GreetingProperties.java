bc3029845d310bb896c56352583054b4d2975dbd
kseni <zaytseva9948@gamil.com>
2026-09-24 16:58:34 +0300

    Chapter 5, Task4

--- greeting-autoconfigure/src/main/java/com/FromPegaToJava/greeting/GreetingProperties.java
+++ greeting-autoconfigure/src/main/java/com/FromPegaToJava/greeting/GreetingProperties.java
@@ -1,13 +1,20 @@
 package com.FromPegaToJava.greeting;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import jakarta.validation.constraints.*;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "greeting")
-
 public class GreetingProperties {
     private boolean enabled = true;
-
-    private String prefix = "Hello";
+    /*
+    @NotBlank vs @NotNull vs @NotEmpty:
+@NotNull — значение не null (но "" пройдёт).
+@NotEmpty — не null и не пустая строка (но " " пройдёт).
+@NotBlank — не null, не пусто, не одни пробелы.
+     */
+    @NotBlank private String prefix;
 
     public boolean isEnabled() { return enabled; }
     public void setEnabled(boolean enabled) { this.enabled = enabled; }
